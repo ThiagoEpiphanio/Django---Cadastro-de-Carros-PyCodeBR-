2ecc63badf7819f1711cbe3a6c99311db2bbc956
Thiago Epiphanio <thiago84.epiphanio@gmail.com>
2025-08-03 15:48:00 -0300

    Aprendendo ORM e QuerySet Django

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from cars.models import Car
 
 
 def cars_view(request):
-    return render(request, 'cars.html')
+    cars = Car.objects.all()
+    
+    return render(
+        request, 
+        'cars.html',
+        {'cars': cars}
+    )

9874053af66737be79072a82d3631c5d110cdbd4
Thiago Epiphanio <thiago84.epiphanio@gmail.com>
2025-08-04 22:13:53 -0300

    Aprendendo Request e Get Django

--- cars/views.py
+++ cars/views.py
@@ -3,7 +3,10 @@ from cars.models import Car
 
 
 def cars_view(request):
-    cars = Car.objects.all()
+    cars = Car.objects.all().order_by('model')
+    search = request.GET.get('search')
+    if search:
+        cars = Car.objects.filter(model_icontains=search)
     
     return render(
         request, 

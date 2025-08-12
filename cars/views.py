0c73e8ce12150ad941e198bb8ef22d6083c4287e
Thiago Epiphanio <thiago84.epiphanio@gmail.com>
2025-08-11 22:36:10 -0300

    Trabalhando com Class Based Views no Django

--- cars/views.py
+++ cars/views.py
@@ -1,28 +1,22 @@
-from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views.generic import ListView, CreateView
 
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
 
-def cars_view(request):
-    cars = Car.objects.all().order_by('model')
-    search = request.GET.get('search')
+    def get_queryset(self):
+        #Super herda da classe principal (ListView)
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
+        if search:
+            cars = cars.filter(model__icontains=search)
+        return cars
 
-    if search:
-        cars = Car.objects.filter(model_icontains=search)
-    
-    return render(
-        request, 
-        'cars.html',
-        {'cars': cars}
-    )
-
-
-def new_car_view(request):
-    if request.method == 'POST':
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-    else:
-        new_car_form = CarModelForm()
-    return render(request, 'new_car.html', { 'new_car_form': new_car_form })
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/cars/'
